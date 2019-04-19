557e4cf6ab1eb0d020effe5d03f8fe24ef9e7988
Josh Beard <jbeard@contractor.usgs.gov>
2019-04-19 15:30:02 -0600

    Use PDK; Update tests

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,49 +1,65 @@
 require 'spec_helper_acceptance'
 
-setup_manifest = <<-EOS
-  if $::operatingsystemmajrelease == '16.04' {
-    $java_home = "/usr/lib/jvm/java-8-openjdk-${::architecture}"
-  } else {
-    $java_home = $::osfamily ? {
-      'RedHat' => '/etc/alternatives/java_sdk',
-      'Debian' => "/usr/lib/jvm/java-7-openjdk-${::architecture}",
-      default  => undef
-    }
-  }
-
-  if !empty('#{CROWD_DOWNLOAD_URL}') {
-    $download_url = '#{CROWD_DOWNLOAD_URL}'
-    notice("CROWD_DOWNLOAD_URL is set. Using ${download_url}")
-  } else {
-    $download_url = undef
-  }
-
-  if !empty('#{CROWD_VERSION}') {
-    $version = '#{CROWD_VERSION}'
-    notice("CROWD_VERSION is set. Using ${version}")
-  } else {
-    $version = undef
-  }
-EOS
-
 describe 'crowd class' do
   context 'default parameters' do
     # Using puppet_apply as a helper
-    it 'should work idempotently with no errors' do
+    it 'works idempotently with no errors' do
       pp = <<-EOS
-        #{setup_manifest}
-        class{'java':}->
-        class { 'crowd':
-          java_home    => $java_home,
-          download_url => $download_url,
-          version      => '2.9.5',
+
+      # Allow for environment variables to specify custom download URLs/versions
+      if !empty('#{CROWD_DOWNLOAD_URL}') {
+        $download_url = '#{CROWD_DOWNLOAD_URL}'
+        notice("CROWD_DOWNLOAD_URL is set. Using ${download_url}")
+      } else {
+        $download_url = undef
+      }
+
+      if !empty('#{CROWD_VERSION}') {
+        $version = '#{CROWD_VERSION}'
+        notice("CROWD_VERSION is set. Using ${version}")
+      } else {
+        $version = undef
+      }
+
+      # Figure out how to install OpenJDK
+      if $::osfamily == 'Debian' {
+        $java_home = "/usr/lib/jvm/java-8-openjdk-${::architecture}"
+        if $::facts['operatingsystem'] == 'Ubuntu' {
+          $package = 'openjdk-8-jdk'
+          $package_options = undef
+        } else {
+          $package = 'openjdk-8-jdk'
+          if $::facts['operatingsystemmajrelease'] == '8' {
+            $package_options = [{'-t' => 'jessie-backports'}]
+          }
         }
+      } elsif $::osfamily == 'RedHat' {
+        $java_home = '/etc/alternatives/java_sdk'
+        $package = 'java-1.8.0-openjdk-devel'
+        $package_options = undef
+      } else {
+        $java_home = undef
+        $package = undef
+        $package_options = undef
+      }
+
+      class { 'java':
+        package         => $package,
+        package_options => $package_options,
+      }
+
+      class { 'crowd':
+				java_home    => $java_home,
+        download_url => $download_url,
+        version      => $version,
+			}
       EOS
 
       # Run it twice and test for idempotency
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes  => true)
+      apply_manifest(pp, catch_failures: true)
       shell('sleep 10')
+      shell('ps aux | grep crowd')
+      apply_manifest(pp, catch_changes: true)
     end
 
     describe service('crowd') do
@@ -53,46 +69,6 @@ describe 'crowd class' do
 
     describe port(8095) do
       it { is_expected.to be_listening }
-    end
-
-    describe command('ps aux | grep -E "java.*atlassian-crowd-2\.9\.5.*Bootstrap start"') do
-      its(:exit_status) { should eq 0 }
-    end
-
-  end
-
-  context 'upgrading' do
-    # Using puppet_apply as a helper
-    it 'should work idempotently with no errors' do
-      pp = <<-EOS
-        #{setup_manifest}
-        class{'java':}->
-        class { 'crowd':
-          java_home    => $java_home,
-          download_url => $download_url,
-          version      => '2.11.1',
-        }
-      EOS
-
-      # Run it twice and test for idempotency
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes  => true)
-      shell('sleep 10')
-
-
-    end
-
-    describe service('crowd') do
-      it { is_expected.to be_enabled }
-      it { is_expected.to be_running }
-    end
-
-    describe port(8095) do
-      it { is_expected.to be_listening }
-    end
-
-    describe command('ps aux | grep -E "java.*atlassian-crowd-2\.11\.1.*Bootstrap start"') do
-      its(:exit_status) { should eq 0 }
     end
   end
 end
